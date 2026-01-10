68f326b5debd609bdbb02523da3765aa5e9f6565
duncte123 <contact@duncte123.me>
2026-01-10 14:20:58 +0100

    First upgrades

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from '@angular/platform-browser';
 import { OWL_DATE_TIME_LOCALE, OptionsTokens } from '../projects/picker/src/public_api';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    {
+    provideZoneChangeDetection(),{
       provide: OWL_DATE_TIME_LOCALE,
       useValue: 'en-US'
     },
