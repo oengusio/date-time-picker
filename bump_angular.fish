e25f705b25c6e05ea57ade3457bca417df4547d6
duncte123 <contact@duncte123.me>
2026-05-08 11:58:32 +0200

    Bump script added

---
+++ bump_angular.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/fish
+
+ng update @angular/common @angular-devkit/build-angular @angular/core @angular/cdk \
+  @angular/compiler @angular/cli @angular-eslint/eslint-plugin-template @angular-eslint/template-parser \
+  angular-eslint eslint
