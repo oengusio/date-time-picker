ec3ccc252b789366ab58d34848186da7c3fdecc0
duncte123 <contact@duncte123.me>
2026-05-08 12:01:08 +0200

    pls allow me to bump

--- bump_angular.fish
+++ bump_angular.fish
@@ -2,4 +2,4 @@
 
 ng update @angular/common @angular-devkit/build-angular @angular/core @angular/cdk \
   @angular/compiler @angular/cli @angular-eslint/eslint-plugin-template @angular-eslint/template-parser \
-  eslint
+  eslint@9 @angular-eslint/eslint-plugin @typescript-eslint/utils @typescript-eslint/types
