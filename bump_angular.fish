3dbaf8aef4894c78b02c8afab420c16a686472f0
duncte123 <contact@duncte123.me>
2026-05-08 11:59:03 +0200

    not dependency removed

--- bump_angular.fish
+++ bump_angular.fish
@@ -2,4 +2,4 @@
 
 ng update @angular/common @angular-devkit/build-angular @angular/core @angular/cdk \
   @angular/compiler @angular/cli @angular-eslint/eslint-plugin-template @angular-eslint/template-parser \
-  angular-eslint eslint
+  eslint
